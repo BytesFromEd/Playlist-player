4f27cf02d1148867c51ec6f7b4fb12e70c9b976f
BytesByEd <edgar.ac.saavedra@gmail.com>
2026-06-15 01:40:45 +0100

    add Github get file to update yt-dlp

---
+++ playlistPlayer.Infrastructure/Github/GithubAsset.cs
@@ -0,0 +1,7 @@
+namespace playlistPlayer.Infrastructure.GithubAsset;
+
+public class GithubAsset
+{
+    public string Name { get; set; } = "";
+    public string Browser_Download_Url { get; set; } = "";
+}
---
+++ playlistPlayer.Infrastructure/Github/GithubRelease.cs
@@ -0,0 +1,6 @@
+namespace playlistPlayer.Infrastructure.GithubAsset;
+
+public class GithubRelease
+{
+    public List<GithubAsset> Assets { get; set; } = [];
+}
