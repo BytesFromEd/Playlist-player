cc847cc98d1f811b8009086334294b916aae43b2
BytesByEd <edgar.ac.saavedra@gmail.com>
2026-08-24 00:54:27 +0100

    missgin file

---
+++ Infrastructure/MediaPlayer/WindowsAudioOutput.cs
@@ -0,0 +1,65 @@
+﻿using NAudio.Wave;
+
+namespace Infrastructure.MediaPlayer;
+
+internal sealed class WindowsAudioOutput : IAudioOutput
+{
+    private readonly WasapiPlayer player;
+
+    public EventHandler<StoppedEventArgs>? OnStopped { get; set; }
+    
+
+    public WindowsAudioOutput()
+    {
+        if (!OperatingSystem.IsWindows())
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        player = new WasapiPlayerBuilder().Build();
+
+        player.PlaybackStopped += (sender, args) => { OnStopped?.Invoke(sender, args); };
+    }
+#pragma warning disable CA1416
+
+    public bool IsPlaying => player.PlaybackState == PlaybackState.Playing;
+    
+    public void Init(IWaveProvider source)
+    {
+        player.Init(source);
+    }
+
+    public void Play()
+    {
+        player.Play();
+    }
+
+    public void Pause()
+    {
+        player.Pause();
+    }
+
+    public void Stop()
+    {
+        player.Stop();
+    }
+
+    public float Volume
+    {
+        get => player.Volume;
+        set => player.Volume = value;
+    }
+
+    public bool Muted
+    {
+        get => player.IsMuted;
+        set => player.IsMuted = value;
+    }
+
+    public void Dispose()
+    {
+        player.Dispose();
+    }
+
+#pragma warning restore CA1416
+}
